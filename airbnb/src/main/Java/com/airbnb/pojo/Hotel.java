5a7a9c0fde11ad50cff9e6657d53892ee6742f8c
Mrzuosi <w584193590@gmail.com>
2018-04-02 16:03:22 +0800

    创建Room类

--- airbnb/src/main/Java/com/airbnb/pojo/Hotel.java
+++ airbnb/src/main/Java/com/airbnb/pojo/Hotel.java
@@ -1,5 +1,13 @@
 package com.airbnb.pojo;
 
 public class Hotel {
+    private  String name;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
